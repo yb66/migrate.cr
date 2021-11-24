40ceccdd228c4dcde575a7f06d1c86514d27016f
iain barnett <helpful-iain@theprintedbird.com>
2021-11-24 21:05:41 +0900

    --wip-- [skip ci]

--- src/migrate/migrator.cr
+++ src/migrate/migrator.cr
@@ -62,15 +62,13 @@ module Migrate
   # migrator.to_latest
   # ```
   class Migrator
-    include Migrate::Migrator::Actions
-    include Migrate::Migrator::SQL
-    include Migrate::Migrator::Validation
-    include Migrate::Migrator::Reporting
-    include Migrate::Migrator::DryRun
-    include Migrate::Migrator::Checksums
-    include Migrate::Migrator::Safety
-    include Migrate::Migrator::VerboseLogging
-    include Migrate::Migrator::EnhancedErrors
+    MIGRATION_FILE_REGEX = /
+      (?<version>\d+)       # e.g. 1 or 19 etc
+      (?: \_                # separator
+        (?<name>\w[\w+\-])  # e.g. first_one or first-one
+      )?                    # It's optional
+      \.sql                 # Extension
+    /x
 
     # The directory migrations were loaded from (nil if using array initialization)
     getter dir : Path | Nil
@@ -105,21 +103,11 @@ module Migrate
     # @param column [String] Name of the version column (default: "version")
     def initialize(
       @db : DB::Database,
-      migrations : Array(Migration),
+      dir : String = "db/migrations",
       @table : String = "migrate_versions",
-      @column : String = "version",
-      adapter : Adapters::Base? = nil
+      @column : String = "version"
     )
-      @adapter = adapter || Adapters::Factory.create(@db)
-      @migrations = {} of String => Migration
-      # Get migrations in order
-      migrations.sort_by {|migration|
-        # Migration raises if no ver
-        migration.version.not_nil!
-      }.each do |migration|
-        @migrations[migration.version.not_nil!] = migration
-      end
-
+      @dir = Path.new(dir).expand
       ensure_version_table_exist
     end
 
@@ -168,8 +156,156 @@ module Migrate
         @migrations[migration.version.not_nil!] = migration
       end
 
-      ensure_version_table_exist
+      raise("There is no version #{target_version} in migrations dir!") unless all_versions.includes?(target_version)
+
+      direction = target_version > current ? Direction::Up : Direction::Down
+
+      applied_versions = all_versions.to_a.select do |version|
+        case direction
+        when Direction::Up
+          version > current && version <= target_version
+        when Direction::Down
+          version - 1 < current && version - 1 >= target_version
+        end
+      end
+
+      case direction
+      when Direction::Up
+        Log.info { "Migrating up to version #{applied_versions.dup.unshift(current.to_i64).map(&.to_s).join(" → ")}" }
+      when Direction::Down
+        # Add previous version to the list of applied versions,
+        # turning "10 → 2" into "10 → 2 → 1"
+        versions = applied_versions.dup.tap do |v|
+          index = all_versions.index(v[0])
+          if index && index > 0
+            v.unshift(all_versions[index - 1])
+          end
+        end
+
+        Log.info { "Migrating down to version #{versions.reverse.map(&.to_s).join(" → ")}" }
+      end
+
+      applied_files = migrations.select do |filename|
+        applied_versions.includes?(MIGRATION_FILE_REGEX.match(filename).not_nil!["version"].to_i64)
+      end
+
+      applied_files.reverse! if direction == Direction::Down
+
+      migrations = applied_files.map { |path|
+        Migration.new(File.join(@dir, path))
+      }
+
+      migrations.each do |migration|
+        if error = migration.error
+          raise error
+        end
+
+        case direction
+        when Direction::Up
+          if error = migration.error_up
+            raise error
+          end
+
+          version = next_version
+          queries = migration.queries_up
+        when Direction::Down
+          if error = migration.error_down
+            raise error
+          end
+
+          version = previous_version
+          queries = migration.queries_down
+        end
+
+        @db.transaction do |tx|
+          if queries.not_nil!.empty?
+            Log.warn { "No queries to run in migration file with version #{version}, applying anyway" }
+          else
+            queries.not_nil!.each do |query|
+              Log.debug { query }
+              tx.connection.exec(query)
+            end
+          end
+
+          Log.debug { update_version_query(version) }
+          tx.connection.exec(update_version_query(version))
+        end
+      end
+
+      previous = current
+      current = current_version
+
+      Log.info { "Successfully migrated from version #{previous} to #{current} in #{TimeFormat.auto(Time.utc - started_at)}" }
+      return current
     end
 
+    private UPDATE_VERSION_SQL = <<-SQL
+    UPDATE %{table} SET %{column} = %{value}
+    SQL
+
+    protected def update_version_query(version)
+      UPDATE_VERSION_SQL % {
+        table:  @table,
+        column: @column,
+        value:  version,
+      }
+    end
+
+    # Return a sorted array of versions extracted from filenames in migrations dir. Contains 0 version which means no migrations.
+    protected def all_versions
+      migrations.map do |filename|
+        MIGRATION_FILE_REGEX.match(filename).not_nil!["version"].to_i64
+      end.unshift(0i64)
+    end
+
+    # Return sorted array of migration file names.
+    protected def migrations
+      Dir.new(@dir).entries.select { |filename|
+        MIGRATION_FILE_REGEX.match(filename)
+      }.sort_by {|filename|
+        MIGRATION_FILE_REGEX.match(filename).not_nil!["version"].to_i64
+      }
+    end
+
+    private CREATE_VERSION_TABLE_SQL = <<-SQL
+    CREATE TABLE IF NOT EXISTS %{table} (%{column} BIGINT NOT NULL)
+    SQL
+
+    private COUNT_ROWS_SQL = <<-SQL
+    SELECT COUNT(%{column}) FROM %{table}
+    SQL
+
+    private INSERT_SQL = <<-SQL
+    INSERT INTO %{table} (%{column}) VALUES (%{value})
+    SQL
+
+    protected def ensure_version_table_exist
+      table_query = CREATE_VERSION_TABLE_SQL % {
+        table:  @table,
+        column: @column,
+      }
+
+      count_query = COUNT_ROWS_SQL % {
+        table:  @table,
+        column: @column,
+      }
+
+      insert_query = INSERT_SQL % {
+        table:  @table,
+        column: @column,
+        value:  0,
+      }
+
+      Log.debug { table_query }
+      @db.exec(table_query)
+
+      Log.debug { count_query }
+      count = @db.scalar(count_query).as(Int64)
+
+      if count == 0
+        Log.debug { insert_query }
+        @db.exec(insert_query)
+      end
+    end
   end
 end
