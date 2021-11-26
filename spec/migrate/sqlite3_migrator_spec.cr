f1e797c17aba330562aa0a4c1fc1a50e1228fc92
iain barnett <helpful-iain@theprintedbird.com>
2021-11-26 17:18:12 +0900

    Separate out Sqlite from PG.

--- spec/migrate/sqlite3_migrator_spec.cr
+++ spec/migrate/sqlite3_migrator_spec.cr
@@ -1,24 +1,9 @@
 require "../spec_helper"
+require "sqlite3"
 
-describe Migrate::Migrator do
-  migration_sql1 = <<-SQL
-  -- Comments that will be skipped
-  -- and ignored
-
-  -- +migrate error Maybe I don't like this.
-
-  -- This never gets run
-  -- +migrate up
-  CREATE TABLE bar (
-    id      integer PRIMARY KEY,
-    content TEXT NOT NULL
-  );
-SQL
-
-  migration_sql2 = <<-SQL
-    -- Comments that will be skipped
-    -- and ignored
-
+describe Migrate::Migration, tags: "sqlite3" do
+  db = DB.open("sqlite3:%3Amemory%3A")
+  migration_sql = <<-SQL
     -- +migrate up
     CREATE TABLE foo (
       id      integer PRIMARY KEY,
@@ -28,36 +13,25 @@ SQL
     -- Indexes
     CREATE UNIQUE INDEX foo_content_index ON foo (content);
 
-    -- From https://www.sqlite.org/fts5.html
-    CREATE TABLE tbl(a INTEGER PRIMARY KEY, b, c);
-
-    CREATE VIRTUAL TABLE fts_idx USING fts5(b, c, content='tbl', content_rowid='a');
-
     -- Statements that might contain semicolons
     -- +migrate start
+    -- From https://www.sqlite.org/fts5.html
+    CREATE TABLE tbl(a INTEGER PRIMARY KEY, b, c);
+    CREATE VIRTUAL TABLE fts_idx USING fts5(b, c, content='tbl', content_rowid='a');
+
     CREATE TRIGGER tbl_ai AFTER INSERT ON tbl BEGIN
       INSERT INTO fts_idx(rowid, b, c) VALUES (new.a, new.b, new.c);
     END;
-    -- +migrate end
 
-    -- +migrate start
     CREATE TRIGGER tbl_ad AFTER DELETE ON tbl BEGIN
       INSERT INTO fts_idx(fts_idx, rowid, b, c) VALUES('delete', old.a, old.b, old.c);
     END;
-    -- +migrate end
 
-    -- +migrate start
     CREATE TRIGGER tbl_au AFTER UPDATE ON tbl BEGIN
       INSERT INTO fts_idx(fts_idx, rowid, b, c) VALUES('delete', old.a, old.b, old.c);
       INSERT INTO fts_idx(rowid, b, c) VALUES (new.a, new.b, new.c);
     END;
     -- +migrate end
-
-    -- +migrate error I really don't like this table.
-    CREATE TABLE bar (
-      id      integer PRIMARY KEY,
-      content TEXT NOT NULL
-    );
 
     -- +migrate down
     DROP trigger tbl_au;
@@ -67,17 +41,48 @@ SQL
     DROP TABLE tbl;
     DROP TABLE foo;
   SQL
-  db = DB.open("sqlite3:%3Amemory%3A")
-  it "does not blow up on instantiation" do
-    migrations = [migration_sql1,migration_sql2]
-                    .zip(1..2)
-                    .map{|sql,i|
-      m = Migrate::Migration.new sql
-      m.version = i.to_s
-      m.name = "spec"
-      m
-    }
-    migrator = Migrate::Migrator.new db, migrations
-    migrator.should_not be_nil
+  migration = Migrate::Migration.new(migration_sql.lines.each)
+  describe "#queries_up" do
+    q1 = <<-SQL
+    CREATE TABLE foo (
+      id      integer PRIMARY KEY,
+      content TEXT NOT NULL
+    );
+    SQL
+
+    q2 = <<-SQL
+    CREATE UNIQUE INDEX foo_content_index ON foo (content);
+    SQL
+
+    q3 = <<-SQL
+    CREATE TABLE tbl(a INTEGER PRIMARY KEY, b, c);
+    SQL
+
+    q4 = <<-SQL
+    CREATE VIRTUAL TABLE fts_idx USING fts5(b, c, content='tbl', content_rowid='a');
+    SQL
+
+    q5 = <<-SQL
+    CREATE TRIGGER tbl_ai AFTER INSERT ON tbl BEGIN
+      INSERT INTO fts_idx(rowid, b, c) VALUES (new.a, new.b, new.c);
+    END;
+    SQL
+
+    q6 = <<-SQL
+    CREATE TRIGGER tbl_ad AFTER DELETE ON tbl BEGIN
+      INSERT INTO fts_idx(fts_idx, rowid, b, c) VALUES('delete', old.a, old.b, old.c);
+    END;
+    SQL
+
+    q7 = <<-SQL
+    CREATE TRIGGER tbl_au AFTER UPDATE ON tbl BEGIN
+      INSERT INTO fts_idx(fts_idx, rowid, b, c) VALUES('delete', old.a, old.b, old.c);
+      INSERT INTO fts_idx(rowid, b, c) VALUES (new.a, new.b, new.c);
+    END;
+    SQL
+
+    it do
+      migration.queries_up.should eq [q1, q2, q3]
+    end
   end
 end
